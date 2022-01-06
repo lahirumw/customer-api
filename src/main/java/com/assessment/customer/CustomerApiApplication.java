479db5e4c125e6ef995b1b0529c8d0f7c7bdc00f
Lahiru Maramba Widanage <lwidanage@wiley.com>
2022-01-06 14:40:21 +0530

    revert test

--- src/main/java/com/assessment/customer/CustomerApiApplication.java
+++ src/main/java/com/assessment/customer/CustomerApiApplication.java
@@ -1,4 +1,5 @@
 package com.assessment.customer;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
