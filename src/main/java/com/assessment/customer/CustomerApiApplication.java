948b011597c1cdf90ded651a39f8ca2e94459139
Lahiru Maramba Widanage <lwidanage@wiley.com>
2022-01-06 14:36:03 +0530

    test

--- src/main/java/com/assessment/customer/CustomerApiApplication.java
+++ src/main/java/com/assessment/customer/CustomerApiApplication.java
@@ -1,5 +1,4 @@
 package com.assessment.customer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
